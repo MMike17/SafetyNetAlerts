b3afc9175306891934ed21689dd92bb6245ea7ae
Mike <mmatthews@manzalab.com>
2021-04-12 14:15:48 +0200

    Added local data file name property in config file

---
+++ src/main/java/com/safetynet/alerts/controller/LocalFileProperty.java
@@ -0,0 +1,18 @@
+package com.safetynet.alerts.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+/**
+ * Configuration file for local file name
+ * 
+ * @author MikeMatthews
+ */
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "com.safetynet.alerts")
+public class LocalFileProperty {
+	String localFileName;
+}
