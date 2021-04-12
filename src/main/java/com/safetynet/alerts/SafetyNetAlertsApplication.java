ad66e355f3a20352729fea19582d6f23b0e71493
Mike <mmatthews@manzalab.com>
2021-04-12 14:23:40 +0200

    Added class to read local file

--- src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -1,13 +1,30 @@
 package com.safetynet.alerts;
 
+import com.safetynet.alerts.controller.LocalDataReader;
+import com.safetynet.alerts.controller.LocalFileProperty;
+import com.safetynet.alerts.model.local.LocalData;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafetyNetAlertsApplication {
+public class SafetyNetAlertsApplication implements CommandLineRunner {
+
+	@Autowired
+	LocalFileProperty localFileProperty;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LocalDataReader localDataReader = new LocalDataReader(localFileProperty.getLocalFileName());
+		LocalData data = localDataReader.readFile();
+
+		if (data != null)
+			System.out.println("Loaded local data from file");
+	}
 }
