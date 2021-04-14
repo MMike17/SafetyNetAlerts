4102f5175508b719d2c5587815b3e7f6d3b24615
Mike <mmatthews@manzalab.com>
2021-04-14 17:19:45 +0200

    Setup test data insertion before tests

--- src/test/java/com/safetynet/alerts/service/PersonServiceIT.java
+++ src/test/java/com/safetynet/alerts/service/PersonServiceIT.java
@@ -1,9 +1,12 @@
 package com.safetynet.alerts.service;
 
 import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.repository.PersonRepository;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -15,17 +18,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PersonServiceIT {
 
+	@Autowired
+	static PersonRepository repository;
+
 	static PersonService testedService;
+	static int nextTestPersonIndex;
 
 	@BeforeAll
 	static void setUp() {
+
 		testedService = new PersonService();
 
-		// SQL objects insertion here
+		Person testPerson = generateTestPerson();
+
+		repository.save(testPerson);
+	}
+
+	@AfterAll
+	static void cleanUp() {
+		nextTestPersonIndex = 0;
+
+		repository.deleteAll();
 	}
 
 	@Test
 	void testPersonSave() {
+
 		// GIVEN
 		// WHEN
 		// THEN
@@ -33,6 +51,7 @@ class PersonServiceIT {
 
 	@Test
 	void testPersonUpdate() {
+
 		// GIVEN
 		// WHEN
 		// THEN
@@ -40,8 +59,24 @@ class PersonServiceIT {
 
 	@Test
 	void testPersonDelete() {
+
 		// GIVEN
 		// WHEN
 		// THEN
+	}
+
+	static Person generateTestPerson() {
+
+		Person testPerson = new Person();
+		testPerson.setId(Long.valueOf(nextTestPersonIndex));
+		testPerson.setFirstName("Test");
+		testPerson.setLastName("TEST");
+		testPerson.setAddress("X Test road");
+		testPerson.setCity("Test city");
+		testPerson.setZipCode(123);
+		testPerson.setPhone("000-000-0001");
+		testPerson.setEmail("testmail@test.com");
+
+		return testPerson;
 	}
 }
