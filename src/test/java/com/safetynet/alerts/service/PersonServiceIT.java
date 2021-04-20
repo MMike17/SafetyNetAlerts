7c65336b15d620717c6a5633014cf274151ca86f
Mike <mmatthews@manzalab.com>
2021-04-20 16:46:04 +0200

    Solved @Autowired test problems

--- src/test/java/com/safetynet/alerts/service/PersonServiceIT.java
+++ src/test/java/com/safetynet/alerts/service/PersonServiceIT.java
@@ -9,118 +9,31 @@ import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.repository.PersonRepository;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * Tests the interractions between PersonService and the database
+ * Tests the interactions between PersonService and the database
  * 
  * @see com.safetynet.alerts.service.PersonService
  * @author MikeMatthews
  */
 @SpringBootTest
-class PersonServiceIT {
+public class PersonServiceIT {
 
-	// @Autowired
-	static PersonRepository repository;
+	@Autowired
+	PersonRepository repository;
 
-	static PersonService testedService;
+	@Autowired
+	PersonService testedService;
+
 	static int nextTestPersonIndex;
-
-	@BeforeAll
-	static void setUp() {
-
-		testedService = new PersonService();
-		repository = new PersonRepository() {
-
-			// Find a way to move all of this away
-			@Override
-			public long count() {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-
-			@Override
-			public void delete(Person arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void deleteAll() {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void deleteAll(Iterable<? extends Person> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void deleteAllById(Iterable<? extends Long> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void deleteById(Long arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public boolean existsById(Long arg0) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public Iterable<Person> findAll() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public Iterable<Person> findAllById(Iterable<Long> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public Optional<Person> findById(Long arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public <S extends Person> S save(S arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public <S extends Person> Iterable<S> saveAll(Iterable<S> arg0) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-		};
-
-		Person testPerson = generateTestPerson();
-
-		repository.save(testPerson);
-	}
 
 	@AfterAll
 	static void cleanUp() {
 
 		nextTestPersonIndex = 0;
-
-		repository.deleteAll();
 	}
 
 	/**
@@ -141,7 +54,7 @@ class PersonServiceIT {
 		if (!succeeded)
 			fail("The repository failed to save the data");
 
-		Optional<Person> resultPerson = repository.findById(Long.valueOf(1));
+		Optional<Person> resultPerson = repository.findById(Long.valueOf(0));
 
 		assertEquals(testPerson, resultPerson.get());
 	}
