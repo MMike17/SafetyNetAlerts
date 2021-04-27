ad0fccb1d143098c9f8bbe8b5edac9cf20e311ba
Mike <mmatthews@manzalab.com>
2021-04-27 17:01:32 +0200

    Fixed unrecognized tests error for FireStation controller

--- src/test/java/com/safetynet/alerts/controller/FireStationControllerIT.java
+++ src/test/java/com/safetynet/alerts/controller/FireStationControllerIT.java
@@ -5,7 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
