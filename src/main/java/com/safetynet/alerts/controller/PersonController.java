6881a1e2cf5b7be30d11a33ca18a0d9e22ea3d2d
Mike <mmatthews@manzalab.com>
2021-04-27 16:36:56 +0200

    Added features to Person controller

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -3,7 +3,6 @@ package com.safetynet.alerts.controller;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.service.PersonService;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,18 +18,18 @@ public class PersonController {
 	@PostMapping("/person")
 	public Person savePerson(Person person) {
 
-		throw new NotImplementedException();
+		return service.addPerson(person);
 	}
 
 	@PutMapping("/person")
 	public boolean updatePerson(Person person) {
 
-		throw new NotImplementedException();
+		return service.updatePersonProfile(person);
 	}
 
 	@DeleteMapping("/person")
 	public boolean deletePerson(Person person) {
 
-		throw new NotImplementedException();
+		return service.removePerson(person.getFirstName(), person.getLastName());
 	}
 }
