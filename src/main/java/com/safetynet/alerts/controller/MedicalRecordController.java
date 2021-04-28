c9293965fe6275fe8e67e02dacf49dc3c86fc28a
Mike <mmatthews@manzalab.com>
2021-04-28 10:13:59 +0200

    Removed useless imports

--- src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
+++ src/main/java/com/safetynet/alerts/controller/MedicalRecordController.java
@@ -3,7 +3,6 @@ package com.safetynet.alerts.controller;
 import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.service.MedicalRecordService;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
