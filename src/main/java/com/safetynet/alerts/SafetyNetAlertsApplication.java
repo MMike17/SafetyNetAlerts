822b7bae4ad11f6abedc907610f68b681f442a2b
Mike <mmatthews@manzalab.com>
2021-05-10 12:16:08 +0200

    Added local data injection in database at startup

--- src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/safetynet/alerts/SafetyNetAlertsApplication.java
@@ -1,7 +1,13 @@
 package com.safetynet.alerts;
 
+import com.safetynet.alerts.controller.FireStationController;
 import com.safetynet.alerts.controller.LocalDataReader;
+import com.safetynet.alerts.controller.MedicalRecordController;
+import com.safetynet.alerts.controller.PersonController;
 import com.safetynet.alerts.model.local.LocalData;
+import com.safetynet.alerts.model.local.LocalFireStation;
+import com.safetynet.alerts.model.local.LocalMedicalRecord;
+import com.safetynet.alerts.model.local.LocalPerson;
 import com.safetynet.alerts.properties.LocalFileProperty;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +21,44 @@ public class SafetyNetAlertsApplication implements CommandLineRunner {
 	@Autowired
 	LocalFileProperty localFileProperty;
 
+	@Autowired
+	PersonController personController;
+
+	@Autowired
+	FireStationController fireStationController;
+
+	@Autowired
+	MedicalRecordController medicalRecordController;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertsApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		LocalDataReader localDataReader = new LocalDataReader(localFileProperty.getLocalFileName());
 		LocalData data = localDataReader.readFile();
 
 		if (data != null)
 			System.out.println("Loaded local data from file");
+		else {
+			System.out.println("Failed to load local data");
+			return;
+		}
+
+		for (LocalPerson person : data.persons) {
+			personController.savePerson(person.toPerson());
+		}
+
+		for (LocalFireStation station : data.firestations) {
+			fireStationController.saveStation(station.toStation());
+		}
+
+		for (LocalMedicalRecord record : data.medicalrecords) {
+			medicalRecordController.saveRecord(record.toRecord());
+		}
+
+		System.out.println("Injected local data in database");
 	}
 }
--- src/main/java/com/safetynet/alerts/model/local/LocalFireStation.java
+++ src/main/java/com/safetynet/alerts/model/local/LocalFireStation.java
@@ -1,12 +1,22 @@
 package com.safetynet.alerts.model.local;
 
+import com.safetynet.alerts.model.FireStation;
+
 /**
  * Class modeling fire stations from local data file
  * 
- * @see com.safetynet.alerts.controller.LocalDataReader
+ * @see LocalDataReader
+ * @see FireStation
+ * 
  * @author MikeMatthews
  */
 public class LocalFireStation {
+
 	public String address;
 	public String station;
+
+	public FireStation toStation() {
+
+		return new FireStation(address, Integer.valueOf(station));
+	}
 }
