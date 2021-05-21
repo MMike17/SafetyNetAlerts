5fecb1e09919126b979260988a6c4ee6cb1ef44d
Mike <mmatthews@manzalab.com>
2021-05-21 19:14:33 +0200

    Fixed bug of non explicit initialization of variable

--- src/main/java/com/safetynet/alerts/model/local/LocalData.java
+++ src/main/java/com/safetynet/alerts/model/local/LocalData.java
@@ -7,9 +7,18 @@ package com.safetynet.alerts.model.local;
  * @see com.safetynet.alerts.model.local.LocalPerson
  * @see com.safetynet.alerts.model.local.LocalFireStation
  * @see com.safetynet.alerts.model.local.LocalMedicalRecord
+ * 
  * @author MikeMatthews
  */
 public class LocalData {
+
+	public LocalData() {
+
+		persons = new LocalPerson[0];
+		firestations = new LocalFireStation[0];
+		medicalrecords = new LocalMedicalRecord[0];
+	}
+
 	public LocalPerson[] persons;
 	public LocalFireStation[] firestations;
 	public LocalMedicalRecord[] medicalrecords;
